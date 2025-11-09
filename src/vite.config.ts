e58fa0acba790e0bc243f7f9215b9c5afd4ffdae
CloudSnowRaccon <behwa7954@gmail.com>
2025-11-09 16:25:57 +0900

    workflows

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig(({ mode }) => ({
-// 사용자 저장소명이 my-todo 라면:
-base: '/HomeWork2/',
-plugins: [react()],
-}))
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/HomeWork2/',
+  plugins: [react()],
+});
