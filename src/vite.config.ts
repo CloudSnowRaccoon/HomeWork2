88b2a34ce2da7b4346a37128068ae8357131e245
CloudSnowRaccon <behwa7954@gmail.com>
2025-11-09 15:59:20 +0900

    Initial commit

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => ({
 // 사용자 저장소명이 my-todo 라면:
-base: '/my-todo/',
+base: '/HomeWork2/',
 plugins: [react()],
 }))
